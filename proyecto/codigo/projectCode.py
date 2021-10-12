dfcbf0629afd8193414f63dd2c2fbd6f03071168
saamayac <88689185+saamayac@users.noreply.github.com>
2021-10-12 15:26:52 -0500

    Update projectCode.py

--- proyecto/codigo/projectCode.py
+++ proyecto/codigo/projectCode.py
@@ -4,20 +4,24 @@
 import urllib.request
 from collections import deque
 import time
+import numpy  as np
+from matplotlib import pyplot as plt
+import math
 
 class Webscraping:
     def __init__(self, url):
         self.__url__ = url
-        self.__arrayName__ = "name"
+        self.__cvsName__ = "name"
 
     def download(self):
         #gets the page code 
-        urllib.request.urlretrieve(self.__url__,self.__arrayName__)
+        urllib.request.urlretrieve(self.__url__,self.__cvsName__)
+        return self.__cvsName__
     
 
     def read (self):
         #reads the page code
-        with open(self.__arrayName__,"r") as firstText:
+        with open(self.__cvsName__,"r") as firstText:
             text = firstText.read()
             return text
 
@@ -58,6 +62,7 @@ class Webscraping:
         return vector
 
 class ImagesCsv:
+    
     def __init__(self):
         self.__baseUrl__ = "https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/datasets/csv/"
         self.__listUrl__ = self.__baseUrl__
@@ -76,36 +81,98 @@ class ImagesCsv:
         self.setImageList(url)
         self.setImage(subUrl)
 
-    def __getImageCsv__(self,subUrl):
+    def getImageCsv(self,subUrl):
         #get the csv file and turns it in to a list compose of lists
         #in other words a matrix returns a matrix of the csv
         self.setImage(subUrl)
         web = Webscraping(self.__url__)
-        web.download()
-        text = web.read()
-        lineas = text.split("\n")
-        list01 = [[int(i) for i in linea if i != ","]for linea in lineas ]
-        return list01
+        name = web.download()
+        matrix01 = np.genfromtxt(name,delimiter = ",", dtype=int)
+        return matrix01
 
-
-    def getImageCsvList(self,url,vector):
-        self.setImageList(url)
-        mainList = deque()
-        for name in vector[0:len(vector)-3]:
-            
-            mainList.insert(0,self.__getImageCsv__(name))
-            #print(self.geturl())
-        return mainList
-            
-
-def main ():
-    cow = Cow()
-    cow.generalCows("enfermo_csv")
-    cow.generalCows("sano_csv")
-
+class CosCompression:
+    def __init__(self):
+        self.quantificationTable =  np.matrix([[16,11,10,16,24,40,51,61],
+                                               [12,12,14,19,26,58,60,55],
+                                               [14,13,16,24,40,57,69,56],
+                                               [14,17,22,29,51,87,80,62],
+                                               [18,22,37,56,68,109,103,77],
+                                               [24,36,55,64,81,104,113,92],
+                                               [49,64,78,87,103,121,120,101],
+                                               [72,92,95,98,112,100,103,99]])
 
     
-class Cow:
+    def matrixNormalization(self,matrix01,flag = True):
+        if flag:
+            matrix01 = np.where(matrix01 > 0,matrix01-128, matrix01-128) #change 
+        else:
+            matrix01 = np.where(matrix01 > 0,matrix01+128, matrix01+128)
+        return matrix01
+
+    def matrixResizing(self, matrix01):
+        size = matrix01.shape
+        if size[0]%8 == 0 and size[1]%8 == 0:
+            return matrix01
+        if size[0]%8 != 0:
+            row = 8-(size[0]-((size[0]//8)*8))
+            for i in range(row):
+                matrix01 = np.r_[matrix01,np.matrix(matrix01[-1])]
+
+        if size[0]%8 != 0:
+            column = 8-(size[1]-((size[1]//8)*8))
+            for i in range(column):
+                matrix01 = np.c_[matrix01,matrix01[:,-1]]
+        
+        return matrix01
+
+    def matrixT(self):
+        list01 = [[ self.C1() if i == 0 else self.C2(j,i) for j in range(8)]for i in range(8)]
+        matrix01 = np.matrix(list01)
+        return matrix01
+        pass
+
+    def C1(self):
+        return round(1/(math.sqrt(8)),4)
+
+    def C2(self,j,i):
+        return round(math.sqrt(1/4)*math.cos(((2*j+1)*i*math.pi)/16),4)
+
+
+    def CDT(self,matrix01):
+        matrixN = self.matrixNormalization(matrix01)
+        matrixD = np.round(np.matmul(np.matmul(self.matrixT(),matrixN),np.transpose(self.matrixT())),1)
+        compressedMatrix = np.round(np.where(matrixD > 0,matrixD/self.quantificationTable, matrixD/self.quantificationTable)).astype(int)
+        return compressedMatrix
+
+    def CDT_Compression(self,matrix01):
+        matrix02 = self.matrixResizing(matrix01)
+        size = matrix02.shape
+
+        for i in range(0,size[0],8):
+            for j in range(0,size[1],8):
+                matrix02[i:i+8,j:j+8] = self.CDT(matrix02[i:i+8,j:j+8])
+        return matrix02
+
+
+
+    def inverseCDT(self,compressedMatrix):
+        size = list(compressedMatrix.shape)
+        matrixN = np.multiply(compressedMatrix,self.quantificationTable)
+        matrixD = np.round(np.matmul(np.matmul(np.transpose(self.matrixT()),matrixN),self.matrixT()),1)
+        decompressedMatrix = self.matrixNormalization(matrixD,False)
+        return decompressedMatrix
+
+    def CDT_Decompression(self,matrix01):
+        matrix02 = self.matrixResizing(matrix01)
+        size = matrix02.shape
+
+        for i in range(0,size[0],8):
+            for j in range(0,size[1],8):
+                matrix02[i:i+8,j:j+8] = self.inverseCDT(matrix02[i:i+8,j:j+8])
+        return matrix02
+
+
+class General:
     def __init__(self):
         self.__baseUrl__ = "https://github.com/mauriciotoro/ST0245-Eafit/tree/master/proyecto/datasets/csv/"
         self.__url__ = self.__baseUrl__
@@ -113,36 +180,82 @@ class Cow:
     def setUrl(self,file):
         self.__url__ = self.__baseUrl__ + file
 
-    def generalCows(self,file):
+    def gg(self,file):
 
         self.setUrl(file)
-
+        
         print("getting links...",end = "")
         web = Webscraping(self.__url__)
         web.download() 
         vector = web.scrappedvector()
         print(" done")
         
-        images = ImagesCsv()
-        print("saving "+  file + "...", end = "")
-        cowsImagesCsv = images.getImageCsvList(file,vector)
+        image = ImagesCsv()
+        print("saving "+  file + "...")
+        image.setImageList(file)
+        cos = CosCompression()
+        
+        for i in (vector[:-3]):#change
+            
+            matrixImagenCsv = image.getImageCsv(i)
+            compresedMatrix = cos.CDT_Decompression(cos.CDT_Compression(matrixImagenCsv))
+
+            '''
+            print (matrixImagenCsv)
+
+            fig = plt.figure(figsize=(32,32))
+            fig.add_subplot(1,2,1)
+            plt.imshow(matrixImagenCsv,cmap = "gray")
+
+            print (compresedMatrix)
+
+            fig.add_subplot(1,2,2)
+            plt.imshow(compresedMatrix, cmap= "gray")
+            '''
+            
+        #plt.show
+            
+
         print(" done")
 
         #code
+        
+        
 
         print("clearing "+  file + "...", end = "")
-        cowsImagesCsv.clear()
+        matrixImagenCsv = 0
         print(" done")
 
+def main ():
+    test02()
 
+def testMain01():
+    gen = General()
+    gen.gg("enfermo_csv")
+    gen.gg("sano_csv")
 
 def test01(vector1,vector2):
     print(vector1)
     Webscraping.specialPrint(vector = vector1)
     Webscraping.specialPrint(vector = vector2)
     
+def test02():
+    gen = General()
+    gen.gg("enfermo_csv")
+    gen.gg("sano_csv")
+
+def test03():
+    url = "https://github.com/mauriciotoro/ST0245-Eafit/tree/master/proyecto/datasets/csv/enfermo_csv/"
+    web = Webscraping(url)
+    web.download() 
+    vector = web.scrappedvector()
+    image = ImagesCsv()
+    image.setImageList("enfermo_csv")
+
+
+    matrix = image.getImageCsv(vector[0])
+    print (matrix[99:105,:5])
 
 
 
 main()
-
